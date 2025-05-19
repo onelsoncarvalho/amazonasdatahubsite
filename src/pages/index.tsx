3172f94ff2de5a6df10923470ec8efd91bddbdf7
Nelson Carvalho <nelsonworkstation@10.0.0.107>
2025-05-18 21:37:14 -0400

    Analytics

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,6 +14,17 @@ export default function Home(): JSX.Element {
     <Layout
       title={`Página Inicial`}
       description={`${siteConfig.tagline}`}>
+      <head>
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-V8XCSGBSKS"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-V8XCSGBSKS');
+        </script>
+      </head>
       <main>
         <HomepageHeader />
         <WhySection />
