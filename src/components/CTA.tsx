373df87c8681609881215c3fa23c9f755d359f57
Nelson Carvalho <nelsonworkstation@10.0.0.107>
2025-05-27 19:48:28 -0400

    CTA Tutorial Button fixed

--- src/components/CTA.tsx
+++ src/components/CTA.tsx
@@ -10,7 +10,7 @@ const CTAButtons: FC<ICTAButtons> = ({ className }) => {
   return (
 
     <div className={`flex gap-4 ${className}`}>
-      <Link to="/docs/tutorial#instalando-o-amazonasdatahub" className={"text-slate-100 bg-cyan-500 rounded-xl border-solid border-transparent shadow-sm shadow-cyan-500/50 text-base cursor-pointer border-1 border-transparent transition duration-200 hover:border-solid hover:border-1 hover:border-teal-400 hover:shadow-lg hover:shadow-cyan-500/50 cursor-pointer"}>
+      <Link to="/docs/instalacao" className={"text-slate-100 bg-cyan-500 rounded-xl border-solid border-transparent shadow-sm shadow-cyan-500/50 text-base cursor-pointer border-1 border-transparent transition duration-200 hover:border-solid hover:border-1 hover:border-teal-400 hover:shadow-lg hover:shadow-cyan-500/50 cursor-pointer"}>
         <button className="w-full h-full bg-transparent border-0 py-2 px-4 text-base cursor-pointer"><Translate id="cta.tutorial">Tutorial</Translate></button>
       </Link>
       <Link to="/docs/dados/overview" className={"text-slate-900 bg-teal-400 rounded-xl shadow-sm shadow-teal-400/50 text-base border-solid border-1 border-transparent shadow-lg shadow-cyan-500/50 text-base cursor-pointer border-1 border-transparent transition duration-200 hover:border-solid hover:border-cyan-400 hover:shadow-lg hover:shadow-teal-500/50 cursor-pointer"}>
