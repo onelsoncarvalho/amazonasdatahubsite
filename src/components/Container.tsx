12fb8c23ba41cdfb60168598a0c3b4edff5bc831
Nelson-DevStack <n.devpro@protonmail.com>
2025-07-28 22:07:25 -0400

    fix: padding from mobile fixed

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -3,7 +3,7 @@ import IComponent from "../interfaces/IComponent"
 
 const Container: FC<IComponent> = ({ children, className }) => {
   return (
-    <div className={`container px-12 mx-auto py-16 h-full grid gap-12 grid-cols-4 md:grid-cols-8 lg:grid-cols-12 ${className}`}>
+    <div className={`container px-8 md:px-12 mx-auto py-16 h-full grid gap-12 grid-cols-4 md:grid-cols-8 lg:grid-cols-12 ${className}`}>
       {children}
     </div>
   )
