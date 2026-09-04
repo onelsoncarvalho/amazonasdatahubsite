dacc123b4d31a4f6aeb948ce1bb2f8c0e65461aa
Nelson-DevStack <n.devpro@protonmail.com>
2026-09-03 22:55:52 -0400

    fix(components): CTA Buttons broken links fixed

--- src/components/CTA.tsx
+++ src/components/CTA.tsx
@@ -10,11 +10,11 @@ const CTAButtons: FC<ICTAButtons> = ({ className }) => {
   return (
 
     <div className={`flex gap-4 ${className}`}>
-      <Link to="/docs" className={"text-slate-100 bg-cyan-500 rounded-xl border-solid border-transparent shadow-sm shadow-cyan-500/50 text-base cursor-pointer border-1 border-transparent transition duration-200 hover:border-solid hover:border-1 hover:border-teal-400 hover:shadow-lg hover:shadow-cyan-500/50 cursor-pointer"}>
-        <button className="w-full h-full bg-transparent border-0 py-2 px-4 text-base cursor-pointer"><Translate id="cta.tutorial">Tutorial</Translate></button>
+      <Link to="/docs/dados" className={"text-slate-100 bg-cyan-500 rounded-xl border-solid border-transparent shadow-sm shadow-cyan-500/50 text-base cursor-pointer border-1 border-transparent transition duration-200 hover:border-solid hover:border-1 hover:border-teal-400 hover:shadow-lg hover:shadow-cyan-500/50 cursor-pointer"}>
+        <button className="w-full h-full bg-transparent border-0 py-2 px-4 text-base cursor-pointer"><Translate id="cta.tutorial">Bases de Dados</Translate></button>
       </Link>
-      <Link to="/docs" className={"text-slate-900 bg-teal-400 rounded-xl shadow-sm shadow-teal-400/50 text-base border-solid border-1 border-transparent shadow-lg shadow-cyan-500/50 text-base cursor-pointer border-1 border-transparent transition duration-200 hover:border-solid hover:border-cyan-400 hover:shadow-lg hover:shadow-teal-500/50 cursor-pointer"}>
-        <button className="w-full h-full bg-transparent border-0 py-2 px-4 text-base cursor-pointer"><Translate id="cta.docs">Ler a Documentação</Translate></button>
+      <Link to="/docs/ensino" className={"text-slate-900 bg-teal-400 rounded-xl shadow-sm shadow-teal-400/50 text-base border-solid border-1 border-transparent shadow-lg shadow-cyan-500/50 text-base cursor-pointer border-1 border-transparent transition duration-200 hover:border-solid hover:border-cyan-400 hover:shadow-lg hover:shadow-teal-500/50 cursor-pointer"}>
+        <button className="w-full h-full bg-transparent border-0 py-2 px-4 text-base cursor-pointer"><Translate id="cta.docs">Ensino</Translate></button>
       </Link>
     </div >
   )
